68977f1dbcd3712d00ef49dae39cb3b21d09534b
dougsack <dougsack@gmail.com>
2024-07-09 16:41:59 -0700

    near v2

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function toggleMenu() {
-    var navLinks = document.getElementById('nav-links');
-    if (navLinks.style.display === 'flex' || navLinks.style.display === '') {
-        navLinks.style.display = 'none';
-    } else {
-        navLinks.style.display = 'flex';
-        navLinks.style.flexDirection = 'column';
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    const modal = document.getElementById("modal");
+
+    // Get the button that opens the modal
+    const btn = document.querySelector(".hero-content button");
+
+    // Get the <span> element that closes the modal
+    const span = document.querySelector(".close");
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "flex";
     }
-}
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
